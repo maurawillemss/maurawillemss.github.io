364759ef634d14e464dd3ce970cc967a02b954ed
maura <98263768+maurawillemss@users.noreply.github.com>
2023-01-25 13:12:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {console.log("loading")
-	navigator.serviceWorker.register('.\sw.js')
+	navigator.serviceWorker.register('sw.js')
 	.then(registratie => {console.log(`Registratie gelukt`)});
 });
 };
